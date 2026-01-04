5503dbb52babcc25e9cb14177cdde4ff3afadb76
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2026-01-03 22:59:07 -0500

    Update Scroll.jsx

--- frontend/src/components/Scroll/Scroll.jsx
+++ frontend/src/components/Scroll/Scroll.jsx
@@ -1,13 +1,12 @@
 // import
 
-
 // css
 
 // function
 export default function Scroll() {
-    return (
-        <div className="scroll">
-            <h1>Welcome to the Scroll!</h1>
-        </div>
-    )
+  return (
+    <div className="scroll">
+      <h1>Welcome to the Scroll!!</h1>
+    </div>
+  );
 }
