dc2d6754d1bd1954f521846454aea0423a7b4f74
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2026-01-04 03:53:03 -0500

    1

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,21 @@
 // import
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 // css
-import "./header.css";
+import './header.css';
 
 // images
-import LogoImg from "../../assets/images/Header/logo.png";
+import LogoImg from '../../assets/images/Header/logo.png';
 
 // function
 export default function Header() {
   return (
     <div className="header">
       <img src={LogoImg} alt="" />
+      <div className="header__wordmark" aria-label="GroundHabit">
+        <span className="header__wordmarkGround">Ground</span>
+        <span className="header__wordmarkHabit">Habit</span>
+      </div>
     </div>
   );
 }
